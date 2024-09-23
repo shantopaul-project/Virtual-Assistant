3f01453c49ba8b3b433d67588301dc89c14aea6e
Shanto Paul <shantopaul9944@gmail.com>
2024-09-23 18:44:07 +0600

    Add function - Shanto's mother name

--- script.js
+++ script.js
@@ -75,6 +75,11 @@ function takeCommand(message){
     }
 
     //Done
+    else if( message.includes("shanto's mother name") || message.includes("mother name")){
+        speak("Shanto's mother name is Sadhana Rani Pal")
+    }
+
+    //Done
     else if( message.includes("time")){
         let time = new Date().toLocaleString(undefined,{hour:"numeric",minute:"numeric"})
         speak(time)
